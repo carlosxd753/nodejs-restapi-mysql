a4bb93d93a3305aad0b1e4ff9be1a5149f4db5bb
carlosxd753 <carlosxd753@gmail.com>
2022-09-30 11:59:00 -0500

    'check'

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
+
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "a95041b3";
 export const DB_HOST = process.env.DB_HOST || "localhost";
