311535cac4883b5c6f1fbba2203e691e4db099e1
carlosxd753 <carlosxd753@gmail.com>
2022-09-30 11:18:59 -0500

    First Commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config();
+
+export const PORT = process.env.PORT || 3000;
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "a95041b3";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_DATABASE = process.env.DB_DATABASE || "companydb";
+export const DB_PORT = process.env.DB_PORT || 3306;
